65daf98039c2f3426584f26025dac0b6fe71a275
liliya-kokhan <58140674+liliya-kokhan@users.noreply.github.com>
2020-10-27 21:57:04 +0200

    Implemented object ID allocator. Refactored TCs (#5)

--- tests/common/switch.py
+++ tests/common/switch.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import redis
 import time
 
@@ -8,10 +9,10 @@ class Sai:
     def __init__(self):
         self.r = redis.Redis(db=1)
 
-
-    def alloc_vid(self):
-        pass
-
+    def alloc_vid(self, obj_type):
+        vid = self.r.incr("VICOUNTER")
+        return hex((obj_type.value << 48) | vid)
+        
     def operate(self, obj, attrs, op):
         self.r.delete("GETRESPONSE_KEY_VALUE_OP_QUEUE")
 
@@ -50,4 +51,98 @@ class Sai:
     def get(self, obj, attrs):
         return self.operate(obj, attrs, "Sget")
 
-
+class SaiObjType(Enum):
+    PORT                     =  1
+    LAG                      =  2
+    VIRTUAL_ROUTER           =  3
+    NEXT_HOP                 =  4
+    NEXT_HOP_GROUP           =  5
+    ROUTER_INTERFACE         =  6
+    ACL_TABLE                =  7
+    ACL_ENTRY                =  8
+    ACL_COUNTER              =  9
+    ACL_RANGE                = 10
+    ACL_TABLE_GROUP          = 11
+    ACL_TABLE_GROUP_MEMBER   = 12
+    HOSTIF                   = 13
+    MIRROR_SESSION           = 14
+    SAMPLEPACKET             = 15
+    STP                      = 16
+    HOSTIF_TRAP_GROUP        = 17
+    POLICER                  = 18
+    WRED                     = 19
+    QOS_MAP                  = 20
+    QUEUE                    = 21
+    SCHEDULER                = 22
+    SCHEDULER_GROUP          = 23
+    BUFFER_POOL              = 24
+    BUFFER_PROFILE           = 25
+    INGRESS_PRIORITY_GROUP   = 26
+    LAG_MEMBER               = 27
+    HASH                     = 28
+    UDF                      = 29
+    UDF_MATCH                = 30
+    UDF_GROUP                = 31
+    FDB_ENTRY                = 32
+    SWITCH                   = 33
+    HOSTIF_TRAP              = 34
+    HOSTIF_TABLE_ENTRY       = 35
+    NEIGHBOR_ENTRY           = 36
+    ROUTE_ENTRY              = 37
+    VLAN                     = 38
+    VLAN_MEMBER              = 39
+    HOSTIF_PACKET            = 40
+    TUNNEL_MAP               = 41
+    TUNNEL                   = 42
+    TUNNEL_TERM_TABLE_ENTRY  = 43
+    FDB_FLUSH                = 44
+    NEXT_HOP_GROUP_MEMBER    = 45
+    STP_PORT                 = 46
+    RPF_GROUP                = 47
+    RPF_GROUP_MEMBER         = 48
+    L2MC_GROUP               = 49
+    L2MC_GROUP_MEMBER        = 50
+    IPMC_GROUP               = 51
+    IPMC_GROUP_MEMBER        = 52
+    L2MC_ENTRY               = 53
+    IPMC_ENTRY               = 54
+    MCAST_FDB_ENTRY          = 55
+    HOSTIF_USER_DEFINED_TRAP = 56
+    BRIDGE                   = 57
+    BRIDGE_PORT              = 58
+    TUNNEL_MAP_ENTRY         = 59
+    TAM                      = 60
+    SEGMENTROUTE_SIDLIST     = 61
+    PORT_POOL                = 62
+    INSEG_ENTRY              = 63
+    DTEL                     = 64
+    DTEL_QUEUE_REPORT        = 65
+    DTEL_INT_SESSION         = 66
+    DTEL_REPORT_SESSION      = 67
+    DTEL_EVENT               = 68
+    BFD_SESSION              = 69
+    ISOLATION_GROUP          = 70
+    ISOLATION_GROUP_MEMBER   = 71
+    TAM_MATH_FUNC            = 72
+    TAM_REPORT               = 73
+    TAM_EVENT_THRESHOLD      = 74
+    TAM_TEL_TYPE             = 75
+    TAM_TRANSPORT            = 76
+    TAM_TELEMETRY            = 77
+    TAM_COLLECTOR            = 78
+    TAM_EVENT_ACTION         = 79
+    TAM_EVENT                = 80
+    NAT_ZONE_COUNTER         = 81
+    NAT_ENTRY                = 82
+    TAM_INT                  = 83
+    COUNTER                  = 84
+    DEBUG_COUNTER            = 85
+    PORT_CONNECTOR           = 86
+    PORT_SERDES              = 87
+    MACSEC                   = 88
+    MACSEC_PORT              = 89
+    MACSEC_FLOW              = 90
+    MACSEC_SC                = 91
+    MACSEC_SA                = 92
+    SYSTEM_PORT              = 93
+    FINE_GRAINED_HASH_FIELD  = 94
--- tests/test_switch.py
+++ tests/test_switch.py
@@ -1,5 +1,7 @@
 import pytest
-from common.switch import Sai
+from common.switch import Sai, SaiObjType
+
+dict_obj_id = {}
 
 @pytest.fixture(scope="module")
 def sai():
@@ -14,4 +16,18 @@ def test_get_default_vrf(sai):
     assert status[2].decode("utf-8") == 'SAI_STATUS_SUCCESS'
     assert status[1].decode("utf-8").split(",")[0][2:-1] == 'SAI_SWITCH_ATTR_DEFAULT_VIRTUAL_ROUTER_ID'
 
+def test_vlan_create(sai):
+    if SaiObjType.VLAN.name not in dict_obj_id:
+        dict_obj_id[SaiObjType.VLAN.name] = {}
+    for vlan in ["100", "200", "300"]:
+        oid = sai.alloc_vid(SaiObjType.VLAN)
+        dict_obj_id[SaiObjType.VLAN.name][vlan] = oid
+        status = sai.create("SAI_OBJECT_TYPE_VLAN:oid:" + oid, '["SAI_VLAN_ATTR_VLAN_ID","'+vlan+'"]')
+        assert status[2].decode("utf-8") == 'SAI_STATUS_SUCCESS'
+
+def test_vlan_remove(sai):
+    for vlan in ["100", "200", "300"]:
+        oid = dict_obj_id[SaiObjType.VLAN.name].pop(vlan)
+        status = sai.remove("SAI_OBJECT_TYPE_VLAN:oid:" + oid)
+        assert status[2].decode("utf-8") == 'SAI_STATUS_SUCCESS'
 
