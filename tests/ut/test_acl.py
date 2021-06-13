1407d89166a72b2a99a6e5d4700734c6b0255d99
Andriy Kokhan <andriy.kokhan@gmail.com>
2021-06-13 21:52:34 +0300

    Added basic UTs for ACL feature (#23)

---
+++ tests/ut/test_acl.py
@@ -0,0 +1,127 @@
+import pytest
+from common.switch import SaiObjType
+
+
+@pytest.fixture(scope="class")
+def acl_state():
+    state = {
+        "table_oid" : "oid:0x0",
+        "entries"   : [],
+    }
+    return state
+
+
+class TestIngressACL:
+    @pytest.mark.dependency()
+    def test_create_table(self, sai, acl_state):
+        attrs = [
+            # ACL table generic attributes
+            "SAI_ACL_TABLE_GROUP_ATTR_ACL_STAGE", "SAI_ACL_STAGE_INGRESS",
+            "SAI_ACL_TABLE_ATTR_ACL_BIND_POINT_TYPE_LIST",
+                    "2:SAI_ACL_BIND_POINT_TYPE_PORT,SAI_ACL_BIND_POINT_TYPE_LAG",
+            # ACL table fields
+            "SAI_ACL_TABLE_ATTR_FIELD_ETHER_TYPE",      "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_OUTER_VLAN_ID",   "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_ACL_IP_TYPE",     "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_SRC_IP",          "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_DST_IP",          "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_ICMP_TYPE",       "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_ICMP_CODE",       "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_IP_PROTOCOL",     "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_L4_SRC_PORT",     "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_L4_DST_PORT",     "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_TCP_FLAGS",       "true",
+            "SAI_ACL_TABLE_ATTR_FIELD_ACL_RANGE_TYPE",
+                    "2:SAI_ACL_RANGE_TYPE_L4_DST_PORT_RANGE,SAI_ACL_RANGE_TYPE_L4_SRC_PORT_RANGE",
+        ]
+        acl_state["table_oid"] = sai.create(SaiObjType.ACL_TABLE, attrs)
+        assert acl_state["table_oid"] != "oid:0x0"
+
+
+    @pytest.mark.parametrize(
+        "match,action,priority",
+        [
+            (
+                [["SAI_ACL_ENTRY_ATTR_FIELD_ETHER_TYPE",        "2048",     "0xffff"]],
+                "SAI_PACKET_ACTION_DROP",       "1"
+            ),
+            (
+                [["SAI_ACL_ENTRY_ATTR_FIELD_SRC_IP",            "20.0.0.2", "255.255.255.255"]],
+                "SAI_PACKET_ACTION_FORWARD",    "9990"
+            ),
+            (
+                [
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_ETHER_TYPE",     "2048",     "0xffff"],
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_SRC_IP",         "20.0.0.4", "255.255.255.255"],
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_IP_PROTOCOL",    "1",        "0xff"],
+                ],
+                "SAI_PACKET_ACTION_FORWARD",    "9988"
+            ),
+            (
+                [
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_ETHER_TYPE",     "2048",     "0xffff"],
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_SRC_IP",         "20.0.0.4", "255.255.255.255"],
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_IP_PROTOCOL",    "17",       "0xff"],
+                ],
+                "SAI_PACKET_ACTION_FORWARD",    "9987"
+            ),
+            (
+                [
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_ETHER_TYPE",     "2048",         "0xffff"],
+                    ["SAI_ACL_ENTRY_ATTR_FIELD_DST_IP",         "192.168.0.8",  "255.255.255.255"],
+                ],
+                "SAI_PACKET_ACTION_DROP",       "9985"
+            ),
+        ]
+    )
+    @pytest.mark.dependency(depends=['TestIngressACL::test_create_table'])
+    def test_create_entry(self, sai, acl_state, match, action, priority):
+        # Create ACL counter
+        counter_attrs = [
+            "SAI_ACL_COUNTER_ATTR_TABLE_ID",            acl_state["table_oid"],
+            "SAI_ACL_COUNTER_ATTR_ENABLE_BYTE_COUNT",   "true",
+            "SAI_ACL_COUNTER_ATTR_ENABLE_PACKET_COUNT", "true",
+        ]
+        counter_oid = sai.create(SaiObjType.ACL_COUNTER, counter_attrs)
+
+        # Create ACL entry
+        entry_attrs = [
+            "SAI_ACL_ENTRY_ATTR_TABLE_ID",              acl_state["table_oid"],
+            "SAI_ACL_ENTRY_ATTR_PRIORITY",              priority,
+            "SAI_ACL_ENTRY_ATTR_ADMIN_STATE",           "true",
+            "SAI_ACL_ENTRY_ATTR_ACTION_PACKET_ACTION",  action,
+        ]
+        for field in match:
+            if field[0] == "SAI_ACL_ENTRY_ATTR_FIELD_ACL_RANGE_TYPE":
+                # TODO: Create SaiObjType.ACL_RANGE object, append field to @entry_attrs
+                continue
+            else:
+                entry_attrs.append(field[0])
+                entry_attrs.append(field[1] + "&mask:" + field[2])
+
+        # Add ACL entry action counter
+        entry_attrs.append("SAI_ACL_ENTRY_ATTR_ACTION_COUNTER")
+        entry_attrs.append(counter_oid)
+
+        status, entry_oid = sai.create(SaiObjType.ACL_ENTRY, entry_attrs, do_assert=False)
+        if status != "SAI_STATUS_SUCCESS":
+            sai.remove(counter_oid)
+        assert status == "SAI_STATUS_SUCCESS"
+
+        # Cache OIDs
+        acl_state["entries"].append(entry_oid)
+        acl_state["entries"].append(counter_oid)
+
+
+    @pytest.mark.dependency(depends=['TestIngressACL::test_create_table'])
+    def test_remove_entries(self, sai, acl_state):
+        if len(acl_state["entries"]) == 0:
+            pytest.skip("no ACL entries to remove")
+        for oid in acl_state["entries"]:
+            sai.remove(oid)
+
+
+    @pytest.mark.dependency(depends=['TestIngressACL::test_create_table'])
+    def test_remove_table(self, sai, acl_state):
+        assert acl_state["table_oid"] != "oid:0x0"
+        sai.remove(acl_state["table_oid"])
