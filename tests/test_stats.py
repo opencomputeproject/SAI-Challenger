6e62899db918e5ad7e60c46bf1844b30aa6709cf
Andriy Kokhan <andriy.kokhan@gmail.com>
2021-10-17 17:22:44 +0300

    Added simple queue stats test (#67)

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,10 +2,10 @@ import pytest
 
 
 def test_stats(npu, dataplane):
-    # Get ports list
-    ports = npu.get(npu.oid, ["SAI_SWITCH_ATTR_PORT_LIST", npu.make_list(33, "oid:0x0")])
 
-    port_oids = ports.oids()
+    # Get ports list
+    port_oids = npu.get_list(npu.oid, "SAI_SWITCH_ATTR_PORT_LIST", "oid:0x0")
+    assert len(port_oids) > 0
 
     # Clear some port stats for port 0
     status = npu.clear_stats(port_oids[0],
@@ -21,9 +21,21 @@ def test_stats(npu, dataplane):
                               'SAI_PORT_STAT_IF_IN_OCTETS', '',
                               'SAI_PORT_STAT_IF_IN_UCAST_PKTS', '',
                               'SAI_PORT_STAT_IF_OUT_OCTETS', ''
-                          ])
-    cntr_list = cntrs.counters()
+                          ]).counters()
 
-    for cntr_id in cntr_list:
-        assert cntr_list[cntr_id] == 0, "{} is not 0".format(cntr_id)
+    for cntr_id in cntrs:
+        assert cntrs[cntr_id] == 0, "{} is not 0".format(cntr_id)
+
+    queue_oids = npu.get_list(port_oids[0], "SAI_PORT_ATTR_QOS_QUEUE_LIST", "oid:0x0")
+    assert len(queue_oids) > 0
+
+    # Get queues stats
+    for queue_oid in queue_oids:
+        cntrs = npu.get_stats(queue_oid,
+                              [
+                                  'SAI_QUEUE_STAT_PACKETS', '',
+                                  'SAI_QUEUE_STAT_BYTES', ''
+                              ]).counters()
+        for cntr_id in cntrs:
+            assert cntrs[cntr_id] == 0, "{} is not 0".format(cntr_id)
 
