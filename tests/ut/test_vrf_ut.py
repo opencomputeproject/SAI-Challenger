13e7d33d94b6508292d52f8b062fe11f6c7648d2
Andriy Kokhan <andriy.kokhan@gmail.com>
2022-05-08 16:52:57 +0300

    Renamed UTs. Added test_vrf_ut.py
    Signed-off-by: Andriy Kokhan <andriy.kokhan@gmail.com>

---
+++ tests/ut/test_vrf_ut.py
@@ -0,0 +1,41 @@
+import pytest
+from sai import Sai
+from sai import SaiObjType
+
+switch_attrs = Sai.get_obj_attrs("SAI_OBJECT_TYPE_VIRTUAL_ROUTER")
+
+
+@pytest.mark.parametrize(
+    "attr,attr_type",
+    switch_attrs
+)
+def test_default_vrf_get_attr(npu, dataplane, attr, attr_type):
+    status, data = npu.get_by_type(npu.default_vrf_oid, attr, attr_type, False)
+    npu.assert_status_success(status)
+
+
+@pytest.fixture(scope="module")
+def vrf_state():
+    state = {
+        "vrf_oid" : "oid:0x0",
+    }
+    return state
+
+@pytest.mark.dependency()
+def test_vrf_create(npu, vrf_state):
+    vrf_state["vrf_oid"] = npu.create(SaiObjType.VIRTUAL_ROUTER, [])
+    assert vrf_state["vrf_oid"] != "oid:0x0"
+
+@pytest.mark.parametrize(
+    "attr,attr_type",
+    switch_attrs
+)
+@pytest.mark.dependency(depends=['test_vrf_create'])
+def test_vrf_get_attr(npu, dataplane, vrf_state, attr, attr_type):
+    status, data = npu.get_by_type(vrf_state["vrf_oid"], attr, attr_type, False)
+    npu.assert_status_success(status)
+
+@pytest.mark.dependency(depends=['test_vrf_create'])
+def test_vrf_remove(npu, vrf_state):
+        assert vrf_state["vrf_oid"] != "oid:0x0"
+        npu.remove(vrf_state["vrf_oid"])
