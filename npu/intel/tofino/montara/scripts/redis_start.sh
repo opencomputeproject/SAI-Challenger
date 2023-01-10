6f8671200c0bb1743e03a46b1a83ffa81fe422bd
Andriy Kokhan <AndriyX.Kokhan@intel.com>
2023-01-10 17:01:18 +0200

    Added support for Intel Montara platform (#31)
    Signed-off-by: Andriy Kokhan <andriyx.kokhan@intel.com>

---
+++ npu/intel/tofino/montara/scripts/redis_start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Script to start Redis using supervisord
+#
+
+if [ $(pgrep -x syncd | wc -l) = "1" ]; then
+    killall -q syncd
+fi
+
+exec /usr/bin/redis-server /etc/redis/redis.conf \
+    $@ --unixsocket /var/run/redis/redis.sock \
+    --pidfile /var/run/redis/redis.pid
