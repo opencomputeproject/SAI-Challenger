90eace79ad764d440d8792ef80e22ac279a6cc5d
Andriy Kokhan <andriy.kokhan@gmail.com>
2023-05-23 12:14:26 +0300

    Fixed testbed name access issue
    Signed-off-by: Andriy Kokhan <andriy.kokhan@gmail.com>

--- tests/test_sairec.py
+++ tests/test_sairec.py
@@ -6,7 +6,7 @@ import time
 def skip_all(testbed_instance):
     testbed = testbed_instance
     if testbed is not None and len(testbed.npu) != 1:
-        pytest.skip("invalid for \"{}\" testbed".format(testbed.meta.name))
+        pytest.skip("invalid for \"{}\" testbed".format(testbed.name))
 
 
 @pytest.fixture(scope="module")
--- tests/ut/test_vrf_ut.py
+++ tests/ut/test_vrf_ut.py
@@ -9,7 +9,7 @@ switch_attrs = Sai.get_obj_attrs("SAI_OBJECT_TYPE_VIRTUAL_ROUTER")
 def skip_all(testbed_instance):
     testbed = testbed_instance
     if testbed is not None and len(testbed.npu) != 1:
-        pytest.skip("invalid for \"{}\" testbed".format(testbed.meta.name))
+        pytest.skip("invalid for \"{}\" testbed".format(testbed.name))
 
 
 @pytest.mark.parametrize(
