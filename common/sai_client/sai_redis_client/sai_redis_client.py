513c6fa3f8b1406b4564e36b2111dd2930552e48
Andriy Kokhan <andriy.kokhan@gmail.com>
2023-05-24 10:14:56 +0300

    Wait upto 120sec on switch init
    Signed-off-by: Andriy Kokhan <andriy.kokhan@gmail.com>

--- common/sai_client/sai_redis_client/sai_redis_client.py
+++ common/sai_client/sai_redis_client/sai_redis_client.py
@@ -109,8 +109,8 @@ class SaiRedisClient(SaiClient):
         status = []
         attempts = self.attempts
 
-        # Wait upto 3 mins for switch init on HW
-        if not self.libsaivs and obj.startswith("SAI_OBJECT_TYPE_SWITCH") and op == "Screate":
+        # Wait upto 3 mins for switch init
+        if obj.startswith("SAI_OBJECT_TYPE_SWITCH") and op == "Screate":
             tout = 0.5
             attempts = 240
 
