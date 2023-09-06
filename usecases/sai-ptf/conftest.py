f1c811e82a8504b4b59dc4ccbca3e65d5568f2d8
Andriy Kokhan <andriy.kokhan@gmail.com>
2023-09-06 17:46:48 +0300

    Updated PTF use-case. Enabled PTF TCs run from CI/CD (#189)
    Signed-off-by: Andriy Kokhan <andriy.kokhan@gmail.com>
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- usecases/sai-ptf/conftest.py
+++ usecases/sai-ptf/conftest.py
@@ -1,18 +1,42 @@
 import sys
 import pytest
+import subprocess
 from saichallenger.common.sai_testbed import SaiTestbedMeta
 
+sys.path.insert(0, '/sai-challenger/ptf/src')
 
-def import_base_modules():
-    sys.path.insert(0, '/sai-challenger/ptf/src')
 
-import_base_modules()
+@pytest.hookimpl(tryfirst=True)
+def pytest_sessionstart(session):
+    patch_file = "/sai-challenger/usecases/sai-ptf/patches/0001-sai-base-test.patch"
+    target_directory = "/sai-challenger/usecases/sai-ptf/SAI/"
+
+    try:
+        command = ["patch", "--dry-run", "--silent", "-N", "-p1", "-i", patch_file, "-d", target_directory]
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        if result.returncode == 0:
+            subprocess.run(["patch", "-p1", "-i", patch_file, "-d", target_directory], check=True)
+        elif result.returncode == 1:
+            # The patch is already applied
+            return
+        else:
+            raise RuntimeError(f"Failed to check whether the patch is already applied: {result}")
+    except Exception as e:
+        raise RuntimeError(f"Failed to apply the patch: {e}")
 
 
 @pytest.fixture(scope="session", autouse=True)
 def set_ptf_params(request):
     if request.config.option.testbed:
         tb_params = SaiTestbedMeta("/sai-challenger", request.config.option.testbed)
+        if tb_params.config['npu'][0]['target'] == 'saivs' and \
+           tb_params.config['npu'][0]['client']['config']['ip'] in ['localhost', '127.0.0.1']:
+            try:
+                # Clean-up saiserver after previous test session
+                subprocess.run(["supervisorctl", "restart", "saiserver"], check=True)
+            except Exception as e:
+                raise RuntimeError(f"Failed to apply the patch: {e}")
+
         tb_params.generate_sai_ptf_config_files()
         ports = to_ptf_int_list(tb_params.config['dataplane'][0]['port_groups'])
     else:
