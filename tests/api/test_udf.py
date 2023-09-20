2c2c95d215974379bcf721be60f7936498142c8e
selldinesh <dinesh.sellappan@keysight.com>
2023-09-20 23:55:20 +0000

    resolving precommit checks
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/api/test_udf.py
+++ tests/api/test_udf.py
@@ -1,6 +1,14 @@
 from pprint import pprint
+import pytest
 
 
+@pytest.fixture(scope='module', autouse=True)
+def skip_all(testbed_instance):
+    testbed = testbed_instance
+    if testbed is not None and len(testbed.npu) != 1:
+        pytest.skip('invalid for {} testbed'.format(testbed.name))
+
+@pytest.mark.npu
 class TestSaiUdf:
     # object with parent SAI_OBJECT_TYPE_UDF_MATCH SAI_OBJECT_TYPE_UDF_GROUP
 
